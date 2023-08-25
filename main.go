d126194a15057a86712238f78ae025cd911f8fa6
Matthew Setter <matthew@matthewsetter.com>
2023-08-25 15:40:47 +0200

    Add the ability to encrypt a file
    This is a larger change than I had intended to create, mainly because
    encryption isn't my area of expertise, nor is it an area that I'm really
    all that familiar with. So this change is what it is. :-(
    
    The change refactors App to store the keyphrase, nonce, and gcm so that
    they're the same throughout the application, and available to all of
    App's methods.
    
    Given that, encryptFileData is now a lot smaller as most of the
    functionality has been moved to the NewApp function. However, the new,
    complimentary decryptFileData method is as small as it can be because it
    only needs to decrypt the supplied byte array and return it, or an
    error, if the text could not be decrypted.
    
    Thanks to these changes, the EncryptFile method has been completed, and
    covering tests have been added to cover most of the available
    functionality. I don't know that I've covered all of the paths through
    the code, yet.
    
    A big thank you has to go to
    https://earthly.dev/blog/cryptography-encryption-in-go/ and
    https://stackoverflow.com/a/11531951/222011 where I found the core of
    the implementation (which was then refactored) and how to actually test
    the implementation.

--- main.go
+++ main.go
@@ -1,19 +1,71 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/joho/godotenv"
 )
 
-type Response struct {
+type ErrorResponse struct {
 	Error   bool
 	Message string
 }
 
+type SuccessResponse struct {
+	OriginalText                            string
+	OriginalTextLength, EncryptedTextLength int64
+	EncryptedText                           []byte
+}
+
 type App struct {
+	key, nonce []byte
+	gcm        cipher.AEAD
+}
+
+func NewApp(keyPhrase []byte) (App, error) {
+	app := App{}
+	aesBlock, err := aes.NewCipher(keyPhrase)
+	if err != nil {
+		return app, fmt.Errorf("could not generate cipher. %v", err)
+	}
+
+	gcmInstance, err := cipher.NewGCM(aesBlock)
+	if err != nil {
+		return app, fmt.Errorf("could not generate a new GCM. %v", err)
+	}
+
+	nonce := make([]byte, gcmInstance.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return app, fmt.Errorf("could not generate a nonce. %v", err)
+	}
+
+	app.key = keyPhrase
+	app.nonce = nonce
+	app.gcm = gcmInstance
+
+	return app, nil
+}
+
+// encryptFileData encrypts text and returns it.
+func (a App) encryptFileData(plainText []byte) []byte {
+	return a.gcm.Seal(nil, a.nonce, plainText, nil)
+}
+
+func (a App) decryptFileData(cipheredText []byte) ([]byte, error) {
+	originalText, err := a.gcm.Open(nil, a.nonce, cipheredText, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not decrypt data. %v", err)
+	}
+	return originalText, nil
 }
 
 // EncryptFile retrieves the encryption type and the file to encrypt from the
@@ -21,17 +73,47 @@ type App struct {
 // the client. Naturally, both arguments must be supplied, otherwise the
 // function cannot worklied, otherwise the function cannot work.
 func (a *App) EncryptFile(writer http.ResponseWriter, request *http.Request) {
-	encryptionType := request.FormValue("encryption_type")
-	if encryptionType == "" {
+	writer.Header().Set("Content-Type", "application/json")
+
+	file, fileHeader, err := request.FormFile("upload_file")
+	if err != nil {
 		writer.WriteHeader(http.StatusBadRequest)
-		writer.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(writer).Encode(Response{
+		json.NewEncoder(writer).Encode(ErrorResponse{
 			Error:   true,
-			Message: `File could not be encrypted as the encryption type is missing.`,
+			Message: `file could not be retrieved as none was supplied.`,
 		})
 
 		return
 	}
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, file); err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(writer).Encode(ErrorResponse{
+			Error:   true,
+			Message: fmt.Sprintf("could not upload file. %v", err),
+		})
+
+		return
+	}
+
+	encryptedText := a.gcm.Seal(a.nonce, a.nonce, buf.Bytes(), nil)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(writer).Encode(ErrorResponse{
+			Error:   true,
+			Message: fmt.Sprintf("could not encrypt file. %v", err),
+		})
+
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(SuccessResponse{
+		OriginalText:        buf.String(),
+		OriginalTextLength:  fileHeader.Size,
+		EncryptedText:       encryptedText,
+		EncryptedTextLength: int64(len(encryptedText)),
+	})
 }
 
 func main() {
@@ -39,7 +121,21 @@ func main() {
 		log.Print("No .env file found.")
 	}
 
-	app := App{}
-	http.HandleFunc("/encrypt", app.EncryptFile)
-	http.ListenAndServe(":8080", nil)
+	// Generate a random 32 byte key for AES-256
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		panic(err.Error())
+	}
+
+	app, err := NewApp(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/encrypt", app.EncryptFile)
+
+	log.Print("Starting server on :4000")
+	err = http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
--- main_test.go
+++ main_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,8 +14,8 @@ import (
 	"github.com/magiconair/properties/assert"
 )
 
-func marshalResponse(isError bool, errorMessage string) (string, error) {
-	expectedBody, err := json.Marshal(Response{
+func marshalErrorResponse(isError bool, errorMessage string) (string, error) {
+	expectedBody, err := json.Marshal(ErrorResponse{
 		Error:   isError,
 		Message: errorMessage,
 	})
@@ -24,22 +26,45 @@ func marshalResponse(isError bool, errorMessage string) (string, error) {
 	return string(expectedBody), nil
 }
 
-func TestEncryptFileMustReceiveEncryptionType(t *testing.T) {
+func marshalSuccessResponse(
+	originalText string,
+	originalTextLength int64,
+	encryptedText []byte,
+	encryptedTextLength int64,
+) (string, error) {
+	expectedBody, err := json.Marshal(SuccessResponse{
+		OriginalText:        originalText,
+		OriginalTextLength:  originalTextLength,
+		EncryptedText:       encryptedText,
+		EncryptedTextLength: encryptedTextLength,
+	})
+	if err != nil {
+		return "", fmt.Errorf("could not marshal expected response body. %v", err)
+	}
+
+	return string(expectedBody), nil
+}
+
+func TestEncryptFileMustReceiveFileToEncrypt(t *testing.T) {
 	// Initialize a new httptest.ResponseRecorder.
 	writer := httptest.NewRecorder()
 
 	// Initialize a new dummy http.Request.
-	request, err := http.NewRequest(http.MethodGet, "/", nil)
+	request, err := http.NewRequest(http.MethodPost, "/encrypt", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+	request.ParseMultipartForm(32 << 20)
 
 	app := App{}
 	app.EncryptFile(writer, request)
 
 	assert.Equal(t, writer.Result().StatusCode, http.StatusBadRequest)
 
-	expectedBody, err := marshalResponse(true, `File could not be encrypted as the encryption type is missing.`)
+	expectedBody, err := marshalErrorResponse(
+		true,
+		`file could not be retrieved as none was supplied.`,
+	)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -51,4 +76,59 @@ func TestEncryptFileMustReceiveEncryptionType(t *testing.T) {
 	}
 	body = bytes.TrimSpace(body)
 	assert.Equal(t, string(body), string(expectedBody))
+}
+
+func TestCanEncryptUploadedFile(t *testing.T) {
+	pr, pw := io.Pipe()
+	writer := multipart.NewWriter(pw)
+
+	fileText := "Here is the test file.\n"
+
+	go func() {
+		defer writer.Close()
+		// We create the form data field 'fileupload'
+		// which returns another writer to write the actual file
+		part, err := writer.CreateFormFile("upload_file", "test-file.txt")
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Fprint(part, fileText)
+	}()
+
+	response := httptest.NewRecorder()
+
+	request, err := http.NewRequest(http.MethodPost, "/encrypt", pr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.ParseMultipartForm(32 << 20)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		t.Fatal(err)
+	}
+
+	app, _ := NewApp(key)
+	app.EncryptFile(response, request)
+
+	decryptedText, err := app.decryptFileData(app.encryptFileData([]byte(fileText)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, response.Result().StatusCode, http.StatusOK)
+
+	defer response.Result().Body.Close()
+	body, err := io.ReadAll(response.Result().Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+	var successResponse SuccessResponse
+	err = json.Unmarshal(body, &successResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, successResponse.OriginalText, string(decryptedText))
 }
