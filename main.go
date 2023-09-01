2e96d06cc3fa0bed41f02cb6d33cebdb084730d4
Matthew Setter <matthew@matthewsetter.com>
2023-09-01 13:21:46 +0200

    Add a middleware function to set security headers
    This function, borrowed directly from Alex Edward's book Let's Go, sets
    the minimum required security headers to each and every request.

--- main.go
+++ main.go
@@ -150,6 +150,16 @@ func (a *App) Encrypt(writer http.ResponseWriter, request *http.Request) {
 	io.WriteString(writer, string(encryptedText))
 }
 
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy", "default-src 'self';")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+		next.ServeHTTP(w, r)
+	})
+}
 
 func (a *App) routes() *http.ServeMux {
 	mux := http.NewServeMux()
